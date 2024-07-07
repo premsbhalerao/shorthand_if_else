b0ffc483e2460ed5d5ce21202aa14de12e45b86f
Prem Bhalerao <sb.prem17@gmail.com>
2024-07-07 20:43:07 +0530

    file add

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String []args){
+        int time = 30;
+        String result = (time<18) ? "Good morning" : "Good evening";
+        System.out.println(result);
+    }}
